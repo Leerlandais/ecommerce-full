7003f46fbc74ea57a0101d39a637df08f1216763
leerlandais <leerlandais@gmail.com>
2024-11-07 19:40:28 +0100

    added more Traits

---
+++ model/Mapping/ProductMapping.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace model\Mapping;
+
+use model\Abstract\AbstractMapping;
+use model\Trait\TraitLaundryRoom;
+
+class ProductMapping extends AbstractMapping
+{
+
+}
---
+++ model/Trait/TraitTestString.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace model\Trait;
+
+trait TraitTestString
+{
+    protected function verifyString (?string $testThis) : bool {
+        if (empty($testThis)) return false;
+        return true;
+    }
+}
