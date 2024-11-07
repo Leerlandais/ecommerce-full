d079d960f15840c4d71895dc472747f703194515
leerlandais <leerlandais@gmail.com>
2024-11-07 16:54:03 +0100

    Setup completed by Leerlandais

---
+++ model/Interface/InterfaceManager.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace model\Interface;
+
+use model\MyPDO;
+use Exception;
+
+use model\Abstract\AbstractMapping;
+
+
+interface InterfaceManager
+{
+    public function __construct(MyPDO $pdo);
+
+}
