007c13cbe550e124da1542c546bc81669d70af97
leerlandais <leerlandais@gmail.com>
2024-11-08 11:33:42 +0100

    added basic user manager

---
+++ model/Manager/UserManager.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace model\Manager;
+
+use model\Abstract\AbstractManager;
+
+class UserManager extends AbstractManager
+{
+    /*
+     * Needs functions for :-
+     *      - Connect User
+     *      - Disconnect User
+     *      - Create User
+     *      - Update User
+     */
+}
