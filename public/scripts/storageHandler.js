c483c37bbe3a0eba6050804cbfc91c68514a0058
leerlandais <leerlandais@gmail.com>
2024-11-10 12:06:16 +0100

    started js rewrite

--- public/scripts/storageHandler.js
+++ public/scripts/storageHandler.js
@@ -1,73 +1,22 @@
 
-
-function createSoldItems(datas) {
-         const storageArray = [];
-   datas.forEach(data => {
-         const item = {id: data.id, amount: data.amount, sold: Math.floor(Math.random() * 40) + 1};
-         storageArray.push(item);
-      localStorage.setItem("SOLD", JSON.stringify(storageArray));
-   });
-      showTest ? logThis("Sold Inventory Created : "+localStorage.getItem("SOLD")) : null;
-}
-
-function addItemToStorage(data) {
-   data = data.split(",")
-    const item = {
-        "id": data[0],
-        "cat": data[1],
-        "item": data[2],
-        "price": data[3],
-        "saved": data[4],
-        "amount": data[5],
-    };
-    // add item to existing basket or create new one if needed
-    showTest ? logThis("Adding "+item["item"]+" to the basket", true) : null;
-    let currentBasket = JSON.parse(localStorage.getItem("BASKET")) || [];
-    showTest ? logThis("Original Basket size is "+currentBasket.length) : null;
-    currentBasket.push(item);
-    localStorage.setItem("BASKET", JSON.stringify(currentBasket));
-    showTest ? logThis("New Basket size is "+JSON.parse(localStorage.getItem("BASKET")).length) : null;
-    if (document.getElementById(`amt${item['id']}`)) {
-        addedItemAdjuster(item)
-    }else {
-        const basket = prepareUnifiedBasket()
-        createCheckoutBasket(basket);
+function createRemainingAmountStorage() {
+    const remainingDisplay = document.querySelectorAll(".remainingDisplay");
+    showTest ? logThis("Creating for "+remainingDisplay.length+" items...") : null;
+    const storage = [];
+    for (let i = 0; i < remainingDisplay.length; i++) {
+       let remaining = remainingDisplay[i].textContent;
+       let id = remainingDisplay[i].id;
+       let data = {
+           id: id,
+           remaining: remaining
+       }
+       storage.push(data);
+       localStorage.setItem("REMAINING", JSON.stringify(storage));
     }
+
+    showTest ? logThis("Remaining Inventory Created : "+localStorage.getItem("REMAINING")) : null;
 }
 
-function deleteOneItemFromStorage(data) {
-    let currentBasket = JSON.parse(localStorage.getItem("BASKET"));
-    const remItem = currentBasket.findIndex(item => item.id === data);
-    currentBasket.splice(remItem, 1);
-    localStorage.setItem("BASKET", JSON.stringify(currentBasket));
-    const basket = prepareUnifiedBasket()
-    createCheckoutBasket(basket);
+if (localStorage.getItem("REMAINING") === null) {
+    createRemainingAmountStorage();
 }
-
-function createNewStorage() {
-    if(confirm("Are you sure you want to reset the Storage?")) {
-        localStorage.clear();
-        location.replace("?route=home");
-
-    }
-}
-
-
-function completeCheckoutOperation() {
-    const finalBasket = prepareUnifiedBasket();
-    const soldDatas = JSON.parse(localStorage.getItem("SOLD"));
-
-    finalBasket.forEach((item) => {
-        for (let i = 0; i < soldDatas.length; i++) {
-            if (parseInt(soldDatas[i].id) === parseInt(item.id)) {
-                soldDatas[i].sold = soldDatas[i].sold + item.occurs;
-                showTest ? logThis("Adjusting sold amount for item: " + soldDatas[i].id + " New value : " + (soldDatas[i].sold + item.occurs)) : null;
-            }
-        }
-    });
-
-    localStorage.setItem("SOLD", JSON.stringify(soldDatas));
-    showTest ? logThis("Removing basket on sale completion : ", true) : null;
-    localStorage.removeItem("BASKET");
-}
-
