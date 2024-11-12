87d28e20acadbd157c977d4149807c39f3d2e87a
leerlandais <leerlandais@gmail.com>
2024-11-12 07:35:13 +0100

    user check : OK

--- Controllers/LoginController.php
+++ Controllers/LoginController.php
@@ -7,9 +7,9 @@ class LoginController extends AbstractController
     public function login()
     {
         global $sessionRole, $errorMessage;
-        if (isset($_POST["userCreateName"], $_POST["userCreatePwd"])) {
-            $name = $_POST["userCreateName"];
-            $pwd = $_POST["userCreatePwd"];
+        if (isset($_POST["userLoginName"], $_POST["userLoginPwd"])) {
+            $name = $_POST["userLoginName"];
+            $pwd = $_POST["userLoginPwd"];
 
             if ($this->userManager->attemptUserLogin($name, $pwd)) {
                 header("Location: ./");
@@ -32,28 +32,24 @@ class LoginController extends AbstractController
     {
         global $sessionRole, $errorMessage;
         if (isset($_POST["userCreateName"],
-                  $_POST["userCreateFullname"],
+                  $_POST["userCreateFullName"],
                   $_POST["userCreateEmail"],
                   $_POST["userCreateAddress"],
                   $_POST["userCreatePwd"],
         )) {
             $name = $_POST["userCreateName"];
-            $fullname = $_POST["userCreateFullname"];
+            $fullname = $_POST["userCreateFullName"];
             $email = $_POST["userCreateEmail"];
             $address = $_POST["userCreateAddress"];
             $pwd = $_POST["userCreatePwd"];
 
             //verify that info is correct and adjust the next bit to handle it instead of login
 
+
             if ($this->userManager->checkForUser($name)) {
+
                 $_SESSION["errorMessage"] = "Username already exists.";
-                header("Location: ./");
-                exit;
-            } else {
-                echo $this->twig->render("public/public.create.html.twig", [
-                    'errorMessage' => $errorMessage,
-                    'sessionRole' => $sessionRole
-                ]);
+                header("Location: ?route=createUser");
             }
         } else {
             echo $this->twig->render("public/public.create.html.twig", [
